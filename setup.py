bcd458dfc44cd63f3f2434887eb0df3fb34d918c
Robert Lacina <robert@lacina.dev>
2023-10-29 05:03:01 +0100

    v2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['bno085_node'],
+    packages=['bno085_node', 'bno085v2_node'],
     package_dir={'': 'src'}
 )
 
